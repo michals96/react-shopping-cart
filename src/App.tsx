e351999cf8cadbeb61fd727050378498c888465a
michals96 <michal.stefaniuk9@gmail.com>
2022-01-04 23:58:14 +0100

    Basic grid without the styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { useQuery } from "react-query";
 import { Wrapper } from "./App.styles";
 
+import Item from './Item/Item';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import Drawer from '@material-ui/core/Drawer';
 import Grid from '@material-ui/core/Grid';
@@ -29,14 +30,22 @@ const App = () => {
 
   const getTotalItems = () => null;
 
-  const handleAddToCart = () => null;
+  const handleAddToCart = (clickedItem: CartItemType) => null;
 
   const handleRemoveFromCart = () => null;
 
   if (isLoading) return <LinearProgress />;
   if (error) return <div>Something went wrong...</div>
 
-  return <div className="App">Start</div>;
+  return <Wrapper>
+    <Grid container spacing={3}>
+      {data?.map((item =>
+        <Grid item key={item.id} xs={12} sm={4}>
+          <Item item={item} handleAddToCart={handleAddToCart}/>
+        </Grid>
+      ))}
+    </Grid>
+  </Wrapper>;
 };
 
 export default App;
