d8cf4ea44952bd8df9010569f37eb5a845ff9c5d
michals96 <michal.stefaniuk9@gmail.com>
2022-01-07 00:22:36 +0100

    Styling for CartItem

--- src/Cart/Cart.styles.ts
+++ src/Cart/Cart.styles.ts
@@ -3,9 +3,5 @@ import styled from "styled-components";
 export const Wrapper = styled.aside`
   font-family: Arial, Helvetica, sans-serif;
   padding: 20px;
-  box-sizing:border-box;
-  display:inline-block;
-  width:200%;
+  width: 500px;
 `;
-//  width:500px;
-
--- src/CartItem/CartItem.tsx
+++ src/CartItem/CartItem.tsx
@@ -1,44 +1,48 @@
-import Button from '@material-ui/core/Button';
+import Button from "@material-ui/core/Button";
 
-import { CartItemType } from '../App';
+import { CartItemType } from "../App";
 
-import { Wrapper } from './CartItem.styles';
-import React from 'react';
+import { Wrapper } from "./CartItem.styles";
+import React from "react";
 
 type Props = {
-    item: CartItemType;
-    addToCart: (clickedItem: CartItemType) => void;
-    removeFromCart: (id: number) => void;
-}
+  item: CartItemType;
+  addToCart: (clickedItem: CartItemType) => void;
+  removeFromCart: (id: number) => void;
+};
 
 const CartItem: React.FC<Props> = ({ item, addToCart, removeFromCart }) => {
-    return (
-        <Wrapper>
-            <h3>{item.title}</h3>
-            <div className="information">
-                <p>Price: ${item.price}</p>
-                <p>Total: ${(item.amount * item.price).toFixed(2)}</p>
-            </div>
-            <div className="buttons">
-                <Button
-                    size="small"
-                    disableElevation
-                    variant="contained"
-                    onClick={() => removeFromCart(item.id)}>
-                    -
-                </Button>
-                <p>{item.amount}</p>
-                <Button
-                    size="small"
-                    disableElevation
-                    variant="contained"
-                    onClick={() => addToCart(item)}>
-                    +
-                </Button>
-            </div>
-            <img src={item.image} title={item.title} />
-        </Wrapper>
-    )
-}
+  return (
+    <Wrapper>
+      <div>
+        <h3>{item.title}</h3>
+        <div className="information">
+          <p>Price: ${item.price.toFixed(2)}</p>
+          <p>Total: ${(item.amount * item.price).toFixed(2)}</p>
+        </div>
+        <div className="buttons">
+          <Button
+            size="small"
+            disableElevation
+            variant="contained"
+            onClick={() => removeFromCart(item.id)}
+          >
+            -
+          </Button>
+          <p>{item.amount}</p>
+          <Button
+            size="small"
+            disableElevation
+            variant="contained"
+            onClick={() => addToCart(item)}
+          >
+            +
+          </Button>
+        </div>
+      </div>
+      <img src={item.image} title={item.title} />
+    </Wrapper>
+  );
+};
 
 export default CartItem;
